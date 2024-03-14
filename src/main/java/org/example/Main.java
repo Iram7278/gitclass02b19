877a8c2e86db63f9d90251ea79ad92c2a2e108e5
Iram7278 <mehmood.iramr@gmail.com>
2024-03-13 18:33:23 -0600

    second commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.print("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello Mars!");
         }
     }
